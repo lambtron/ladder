6055876b08a7f6978f6a272da44ec0c6d141aacd
Andy Jiang <andyjiang@Andys-MacBook-Pro.local>
2015-03-14 11:10:31 -0700

    derping more

--- client/js/button.js
+++ client/js/button.js
@@ -1,12 +1,29 @@
+/**
+ * Module dependencies.
+ */
+
 import {component,dom} from 'deku';
 
-let Button = component({
-  onClick(e) {
-    console.log('Clicked!');
-  },
-  render() {
-    return dom('button', { onClick: this.onClick }, 'Click me');
-  }
-});
+/**
+ * Define `Button`.
+ */
 
-export Button;
+var Button = component();
+
+/**
+ * Expose `Button`.
+ */
+
+Button.prototype.render = function(props, state) {
+
+  /**
+   * Click handler.
+   */
+
+  function onClick(e){
+   console.log('Clicked!');
+  }
+
+  return dom('button', { onClick: this.onClick }, 'Click me');
+};
+
