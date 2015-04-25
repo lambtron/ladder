beaf45d37520d61c576d337c9013a63424bab0ce
Andy Jiang <andyjiang@gmail.com>
2015-04-25 14:00:39 -0700

    updated server/lib/elo.js

--- server/lib/elo.js
+++ server/lib/elo.js
@@ -5,6 +5,7 @@
 
 var elo = require('elo-rank')(15);
 var User = require('./user');
+var Game = require('./game');
 
 /**
  * Re-rank.
@@ -17,13 +18,32 @@ module.exports = function *(w, l) {
   var winner = yield User.findOne({ name: w });
   var loser = yield User.findOne({ name: l });
   if (!winner || !loser) throw 'No user';
-  var expectedScoreWinner = elo.getExpected(winner.rating, loser.rating);
-  var expectedScoreLoser = elo.getExpected(loser.rating, winner.rating);
-  winner.rating = elo.updateRating(expectedScoreWinner, 1, winner.rating);
+  var game = {
+    winner: { name: winner.name, old: winner.rating },
+    loser: { name: loser.name, old: loser.rating }
+  };
+  var ratings = getNewRatings(winner.rating, loser.rating);
+  winner.rating = ratings.winner;
+  loser.rating = ratings.loser;
   winner.games++;
-  loser.rating = elo.updateRating(expectedScoreLoser, 0, loser.rating);
   loser.games++;
+  game.winner.new = winner.rating;
+  game.loser.new = loser.rating;
   User.update({ name: winner.name }, winner);
   User.update({ name: loser.name }, loser);
+  yield Game.create(game);
   return;
 };
+
+/**
+ * Get new ratings.
+ */
+
+function getNewRatings(winner, loser) {
+  var expectedScoreWinner = elo.getExpected(winner, loser);
+  var expectedScoreLoser = elo.getExpected(loser, winner);
+  return {
+    winner: elo.updateRating(expectedScoreWinner, 1, winner),
+    loser: elo.updateRating(expectedScoreLoser, 0, loser)
+  };
+}
